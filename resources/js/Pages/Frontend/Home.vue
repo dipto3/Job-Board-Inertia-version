7a3941f66152c2f86378483664536417406d4fdf
Piyal Guho Dipto <diptoguho4@gmail.com>
2024-07-03 21:41:40 +0600

    jobs

--- resources/js/Pages/Frontend/Home.vue
+++ resources/js/Pages/Frontend/Home.vue
@@ -27,9 +27,9 @@
                                     placeholder="web development,10000-20000,Dhaka">
                             </div>
 
-                            <!-- {{-- <div class="col-lg-auto col-sm-6 col-12 flex-grow-1 mb-4">
+                            <div class="col-lg-auto col-sm-6 col-12 flex-grow-1 mb-4">
                                 <button class="btn btn-primary">Search</button>
-                            </div> --}} -->
+                            </div>
 
                         </div>
                     </form>
@@ -49,7 +49,38 @@
                 <p>Here's the most recent job listed on the website.</p>
             </div>
 
-            <livewire:homefeed />
+            <!-- <livewire:homefeed /> -->
+            <div class="job-list-wrap">
+
+<!-- @foreach ($activeJobs as $job) -->
+    <!-- Job List Start -->
+    <a href="{{ route('job.description', $job->uuid) }}" class="job-list row">
+        <div class="company-logo col-auto">
+            <img src="frontend/assets/images/companies/company-1.png" alt="Company Logo">
+        </div>
+        <div class="salary-type col-auto order-sm-3">
+            <span class="salary-range">{{ $job->salary }}</span>
+            <span class="badge bg-success" style="text-transform: uppercase;">{{ $job->employment_type }}</span>
+        </div>
+        <div class="content col">
+            <h6 class="title">{{ $job->title }}</h6>
+            <ul class="meta">
+                <li><strong class="text-primary">{{ $job->user->name }}</strong></li>
+                <li><i class="fa fa-map-marker"></i> {{ $job->location }}</li>
+           <li><i class="fa fa-map-marker"></i> {{ $job->user->companyInfo->contract_number }}</li> 
+            </ul>
+        </div>
+    </a>
+    <!-- Job List Start -->
+<!-- @endforeach -->
+
+
+</div>
+<!-- Job List Wrap Start -->
+
+<div class="text-center mt-4 mt-lg-5">
+<button wire:click="loadMore" class="btn btn-primary">View All Jobs</button>
+</div>
 
         </div>
     </div>
